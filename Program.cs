19c43683e88397d10b4660882f68bda8e144d8d9
purpledragon89 <rileywtaylor@gmail.com>
2025-01-15 17:03:12 -0700

    m

--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using MissionTwo;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        //welcome message and get roll number
+        Console.WriteLine("Welcome to the dice throwing simulator! \n How many dice rolls would you like to simulate? ");
+        int num_rolls = int.Parse(System.Console.ReadLine());
+        
+        //call the method of the second class to do calculations
+        SecondClass sc = new SecondClass();
+        int[] result = sc.SecondMethod(num_rolls);
+        
+        //calculate and print
+        Console.WriteLine("DICE ROLLING SIMULATION RESULTS\nEach \"*\" represents 1% of the total number of rolls.\nTotal number of rolls = " + num_rolls);
+        int number = 2;
+        int number2 = 0;
+        for(int i = 0; i < result.Length; i++)
+        {
+            double starsCount = (result[number2] / (double)num_rolls) * 100;
+            string stars = new string('*', Math.Max(1, (int)starsCount));
+            Console.WriteLine(number + ": " + stars);
+            number++; 
+            number2++;
+        }
+
+        Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+
+    }
+}
+
+//get number of rolls
+//store the number
+
+//roll one die ande store the number
+//roll the other die and store the number
+//add the two numbers together
+//update the tally that cooresponds to that number
+
+
+//for each number
+//calculate its percentage out of 100
+
+//print the number 1-12
+//for each percentage , display the asterisks
--- SecondClass.cs
+++ SecondClass.cs
@@ -2,5 +2,21 @@ namespace MissionTwo;
 
 public class SecondClass
 {
-    
+    public int[] SecondMethod(int num_rolls)
+    {
+        //Make random numbers
+        Random rnd = new Random();
+        int[] results = new int[11];
+        
+        for (int i = 0; i < num_rolls; i++)
+        {
+            int num1 = rnd.Next(1, 7);
+            int num2 = rnd.Next(1, 7);
+            
+            //add the 2 random numbers together
+            int roll_result = num1 + num2;
+            results[roll_result - 2] += 1;
+        }
+        return results;
+    }
 }
